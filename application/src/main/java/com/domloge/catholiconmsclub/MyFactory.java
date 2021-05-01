2692e9a6a24159e4c03694032b3162a5f8a91686
Ramsay Domloge <rdomloge@gmail.com>
2021-05-01 01:13:24 +0100

    Another attempt

--- application/src/main/java/com/domloge/catholiconmsclub/MyFactory.java
+++ application/src/main/java/com/domloge/catholiconmsclub/MyFactory.java
@@ -5,11 +5,16 @@ import javax.inject.Singleton;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.context.annotation.Factory;
 
 
 @Factory
 public class MyFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClubConfiguration.class);
 
     private final ClubConfiguration config;
     
@@ -19,6 +24,8 @@ public class MyFactory {
 
     @Singleton
     public MongoClient mongoClient() {
-        return MongoClients.create(config.getConnectionUri());
+        String connectionUri = config.getConnectionUri();
+        LOGGER.info("Connection URI: {}", connectionUri);
+        return MongoClients.create(connectionUri);
     }
 }
