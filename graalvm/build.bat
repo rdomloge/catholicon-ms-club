120364c233dbe8e2655460b014f01724aa369fca
Ramsay Domloge <rdomloge@gmail.com>
2021-05-07 11:50:30 +0100

    Slimming down the improving the build process

--- graalvm/build.bat
+++ graalvm/build.bat
@@ -1,6 +1,5 @@
-docker buildx build --no-cache --platform linux/arm64 --build-arg ARCH=aarch64 -t rdomloge/graalvm-21.1.0-java11:arm64 . --push
-
 docker buildx build --no-cache --platform linux/amd64 --build-arg ARCH=amd64 -t rdomloge/graalvm-21.1.0-java11:amd64 . --push
+docker buildx build --no-cache --platform linux/arm64 --build-arg ARCH=aarch64 -t rdomloge/graalvm-21.1.0-java11:arm64 . --push
 
 docker manifest rm rdomloge/graalvm-21.1.0-java11:multiarch
 docker manifest create rdomloge/graalvm-21.1.0-java11:multiarch rdomloge/graalvm-21.1.0-java11:arm64 rdomloge/graalvm-21.1.0-java11:amd64
