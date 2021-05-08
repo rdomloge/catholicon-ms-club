f4899bc0d9464d7e1b28f7849dbf964d165a175a
Ramsay Domloge <rdomloge@gmail.com>
2021-05-08 18:29:18 +0100

    Another attempt at finding the culprit

--- application/src/main/java/com/domloge/catholiconmsclub/MyFactory.java
+++ application/src/main/java/com/domloge/catholiconmsclub/MyFactory.java
@@ -5,17 +5,14 @@ import javax.inject.Singleton;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 
+import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
-import io.micronaut.context.annotation.Value;
 
 @Factory
 public class MyFactory {
-
-    @Value("${mongoHost:defaultNotSpecified}")
-    protected String host;
     
     @Singleton
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://admin:password@"+host+":27017");
+        return MongoClients.create("mongodb://admin:password@10.0.0.15:27017");
     }
 }
