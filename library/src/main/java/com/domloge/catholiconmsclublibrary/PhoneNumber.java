f5864542f3bb5730486ec1a5c343684b67ce0090
Ramsay Domloge <rdomloge@gmail.com>
2021-05-19 15:05:57 +0100

    asdf

--- library/src/main/java/com/domloge/catholiconmsclublibrary/PhoneNumber.java
+++ library/src/main/java/com/domloge/catholiconmsclublibrary/PhoneNumber.java
@@ -5,33 +5,32 @@ import io.micronaut.core.annotation.Introspected;
 @Introspected
 public class PhoneNumber {
 	
-	// private PhoneNumberType type;
+	private PhoneNumberType type;
 	
 	private String number;
 
 	public PhoneNumber() {
 	}
 
-	public PhoneNumber(/*PhoneNumberType type,*/ String number) {
+	public PhoneNumber(PhoneNumberType type, String number) {
 		super();
-		// this.type = type;
+		this.type = type;
 		this.number = number;
 	}
 
-	// public PhoneNumberType getType() {
-	// 	return type;
-	// }
+	public PhoneNumberType getType() {
+		return type;
+	}
 
 	public String getNumber() {
 		return number;
 	}
 
-	// public void setType(PhoneNumberType type) {
-	// 	this.type = type;
-	// }
+	public void setType(PhoneNumberType type) {
+		this.type = type;
+	}
 
 	public void setNumber(String number) {
 		this.number = number;
 	}
-
 }
